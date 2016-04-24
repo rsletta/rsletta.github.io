1d4114df35059359d246db5c12c920bacb1e49d7
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2016-04-24 19:53:51 +0200

    no message

--- imageBrowser/js/script.js
+++ imageBrowser/js/script.js
@@ -1,3 +1,6 @@
+var slid_event = $.Event('slid.bs.carousel', { relatedTarget: relatedTarget, direction: direction }) // yes, "slid"
+console.log(slid_event)
+
 $('#carousel').on('slid.bs.carousel', function () {
   console.log("Switch image")
 })
