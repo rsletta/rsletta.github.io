b8a068f0cf8f62db9a91ff6eea5c7365c3d86f2b
Ronnie André Bjørvik Sletta <rsletta@rikosjett.no>
2016-04-24 15:10:46 +0200

    Implemented ImageBrowser Hack

--- imageBrowser/js/script.js
+++ imageBrowser/js/script.js
@@ -1,3 +1,21 @@
-$('#carousel').on('slid.bs.carousel', function () {
-  console.log("Switch image")
-})
+$(document).ready(function(){
+  $('#carousel').on('slid.bs.carousel', function (e) {
+    var activeImage = $('.carousel-inner').find(".item.active").children();
+    var activeImageId = "#" + activeImage[0].attributes.thumb.value
+    // Remove active thumbnail
+    $('.thumbnails').children().find('.active-thumb').removeClass('active-thumb');
+    // Set new active thumbnail
+    $('.thumbnails').children().find(activeImageId).addClass('active-thumb');
+  })
+
+  $('.thumbs').find('div').click(function(e) {
+    var clickedThumb = $(this).children();
+    // Remove active thumbnail
+    $('.thumbs').children().find('.active-thumb').removeClass('active-thumb');
+    // Set this at active thumbnail
+    $(this).addClass('active-thumb');
+    // Change image
+    var imgUrl = clickedThumb[0].attributes.src.value;
+    $( '#active-image' ).attr('src', imgUrl)
+  });
+});
